3283415a3a3469c2d4e381fff462fbc3a25ab29a
Ashley Kumar <askumar@andrew.cmu.edu>
2014-01-16 21:42:05 -0500

    better render

--- testsite/testsite/views.py
+++ testsite/testsite/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
+from django.shortcuts import render
 
 import datetime
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    t = get_template('current_datetime.html')
-    html = t.render(Context({'current_date': now}))
-    return HttpResponse(html)
+    #t = get_template('current_datetime.html')
+    #html = t.render(Context({'current_date': now}))
+    #return HttpResponse(html)
+    #even better, don't have toe get the template and make a context
+    return render(request, 'current_datetime.html', {'current_date' : now})
 
 
 def hello(request):
