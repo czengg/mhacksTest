2b7ce2277b0a6dfa93ae25c3cfdac71c09b92503
Ashley Kumar <askumar@andrew.cmu.edu>
2014-01-16 20:39:37 -0500

    made a template bitch

--- testsite/testsite/urls.py
+++ testsite/testsite/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from testsite.views import hello
+from testsite.views import hello, current_datetime
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'testsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),
     url(r'^admin/', include(admin.site.urls)),
 )
--- testsite/testsite/views.py
+++ testsite/testsite/views.py
@@ -1,4 +1,15 @@
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+
+import datetime
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    t = get_template('current_datetime.html')
+    html = t.render(Context({'current_date': now}))
+    return HttpResponse(html)
+
 
 def hello(request):
     return HttpResponse("Hello world! See I'm not *that* bad at git")
